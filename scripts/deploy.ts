833ac6df73ca60bb4a17c783d10829e84f733281
jonathan gregis <jon@ip-192-168-0-85.us-east-2.compute.internal>
2022-12-20 21:47:58 -0600

    add network config

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,31 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
+import { networkConfig } from  "../network.config"
+import { deploy } from "../test/utils/helpers";
 
 async function main() {
-  const BM = await ethers.getContractFactory("VRFBalanceManger");
-  const bm = await BM.deploy();
+  const chainId = network.config.chainId != undefined ? network.config.chainId : 31337;
+  const networkName = {
+    name: networkConfig[chainId].name,
+    keepersUpdateInterval: networkConfig[chainId].keepersUpdateInterval,
+    linkTokenERC677: networkConfig[chainId].linkTokenERC677,
+    vrfCoordinatorV2: networkConfig[chainId].vrfCoordinatorV2,
+    keepersRegistry: networkConfig[chainId].keepersRegistry,
+    minWaitPeriodSeconds: networkConfig[chainId].minWaitPeriodSeconds,
+    dexAddress: networkConfig[chainId].dexAddress,
+    linkContractBalance: networkConfig[chainId].linkContractBalance,
+    erc20AssetAddress: networkConfig[chainId].erc20AssetAddress
+  }
+  const bm = await deploy("VRFBalancer", [networkName.linkTokenERC677,
+    networkName.vrfCoordinatorV2,
+    networkName.keepersRegistry,
+    networkName.minWaitPeriodSeconds,
+    networkName.dexAddress,
+    networkName.linkContractBalance,
+    networkName.erc20AssetAddress]);
 
   await bm.deployed();
 
-  console.log(`Circuit Breaker deployed to ${bm.address}`);
+  console.log(`VRF Balance Manager deployed to ${bm.address}`);
 }
 
 main().catch((error) => {
